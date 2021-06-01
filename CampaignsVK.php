82c8b6d2f09395a8a514f0b13c51bd99c4edbd44
denkeel <denkeelpro@gmail.com>
2021-06-02 02:55:09 +0300

    Добавил вывод объявлений

--- CampaignsVK.php
+++ CampaignsVK.php
@@ -44,4 +44,28 @@ class CampaignsVK implements ICampaigns
 
         return $standardizedCampaigns;
     }
+
+    public function getAds() {
+        $vkApi = new ApiRequestVK($this->apiVersion);
+
+        $methodName = "ads.getAds";
+        $parameters = ["account_id" => $this->accountId];
+
+        $response = $vkApi->makeRequest($methodName, $parameters, $this->accessToken);
+
+        return $this->standardizeAdsData($response);
+    }
+
+    public function standardizeAdsData(string $responseJson)
+    {
+        $response = json_decode($responseJson);
+        $ads = $response->response;
+        
+        $standardizedAds = [];
+        foreach($ads as $ad) {
+            $standardizedAds[$ad->campaign_id][] = ['ad_id' => $ad->id, 'ad_name' => $ad->name];
+        }
+
+        return $standardizedAds;
+    }
 }

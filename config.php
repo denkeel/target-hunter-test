23397ecccbdf67cffceed36e3067aeb7f63687d2
denkeel <denkeelpro@gmail.com>
2021-06-02 02:51:43 +0300

    Добавил Config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return (object) [
+    'vkClientId' => '7869412',
+    'vkClientSecret' => 'pslNe8ildsngtOdLC2lZ',
+    'vkApiVersion' => '5.131',
+
+    'redirectUri' => 'http://denkeel.cf/',
+];
